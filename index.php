a0f5feddcfb2090db13632e06d83a62afdfc6431
Daniel Vedin <dv222bk@student.lnu.se>
2015-10-18 15:34:08 +0200

    Initial Commit
    Added the initial files needed for the project.

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    require_once('model/Map.php');
+	require_once('model/MapTile.php');
+	require_once('model/dal/MapDAL.php');
+	
+	require_once('controller/Controls.php');
+	require_once('controller/MapController.php');
+	
+	require_once('view/LayoutView.php');
+	require_once('view/MapView.php');
+	require_once('view/ControlsView.php');
+	
+	$mapDAL = new \model\MapDAL();
+	$map = new \model\Map($mapDAL);
+	$layoutView = new \view\LayoutView();
+	$mapView = new \view\MapView();
+	$controlsView = new \view\ControlsView();
+	$controls = new \controller\Controls($controlsView);
+	$mapController = new \controller\MapController($map, $mapView);
